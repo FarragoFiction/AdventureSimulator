c3cd2c6a1ba40387040cecfd2faa70d0ba63ecaa
JR <jadedResearcher@gmail.com>
2022-02-03 20:24:32 -0500

    style is perfection

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="story-so-far">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut suscipit libero. Ut sapien arcu, tristique ut tristique sit amet, accumsan ut erat. Fusce et turpis volutpat nulla vulputate aliquet. Duis ullamcorper magna nec semper egestas. Pellentesque ac elementum elit. Aliquam erat volutpat. Curabitur sollicitudin dolor eros, eget malesuada tellus pulvinar at. Integer ultricies turpis ut tortor gravida volutpat. Curabitur nec porta augue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis consectetur neque fringilla imperdiet finibus. Integer mattis tincidunt iaculis. Fusce sodales, eros quis porta maximus, justo ipsum tincidunt felis, quis lobortis nibh justo nec mi. Morbi non consectetur magna. Aenean a interdum urna.
+        <br></br><br></br>
+        Donec bibendum enim vitae lectus hendrerit, vitae porta nisi porta. Sed at rhoncus nisi. Duis posuere ipsum a orci convallis ultricies nec sed dui. Donec non malesuada ante. Aenean ut lacus vitae eros hendrerit porta. Curabitur auctor risus sed enim semper, eu condimentum velit elementum. Sed eu nibh imperdiet, varius augue vel, congue dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce semper, justo a maximus tempor, lacus neque cursus dui, condimentum blandit lorem neque nec nulla. Vestibulum tincidunt malesuada leo, in iaculis lectus sagittis eget. Maecenas facilisis quam in justo tincidunt, vel maximus massa vulputate. Cras vehicula, eros ut malesuada varius, sem eros dapibus nulla, nec lobortis arcu quam quis nisi. Donec nisl arcu, sollicitudin id nulla id, ultrices porta purus. Quisque tempor condimentum efficitur. Nullam suscipit rutrum scelerisque.
+
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut suscipit libero. Ut sapien arcu, tristique ut tristique sit amet, accumsan ut erat. Fusce et turpis volutpat nulla vulputate aliquet. Duis ullamcorper magna nec semper egestas. Pellentesque ac elementum elit. Aliquam erat volutpat. Curabitur sollicitudin dolor eros, eget malesuada tellus pulvinar at. Integer ultricies turpis ut tortor gravida volutpat. Curabitur nec porta augue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis consectetur neque fringilla imperdiet finibus. Integer mattis tincidunt iaculis. Fusce sodales, eros quis porta maximus, justo ipsum tincidunt felis, quis lobortis nibh justo nec mi. Morbi non consectetur magna. Aenean a interdum urna.
+        <br></br><br></br>
+        Donec bibendum enim vitae lectus hendrerit, vitae porta nisi porta. Sed at rhoncus nisi. Duis posuere ipsum a orci convallis ultricies nec sed dui. Donec non malesuada ante. Aenean ut lacus vitae eros hendrerit porta. Curabitur auctor risus sed enim semper, eu condimentum velit elementum. Sed eu nibh imperdiet, varius augue vel, congue dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce semper, justo a maximus tempor, lacus neque cursus dui, condimentum blandit lorem neque nec nulla. Vestibulum tincidunt malesuada leo, in iaculis lectus sagittis eget. Maecenas facilisis quam in justo tincidunt, vel maximus massa vulputate. Cras vehicula, eros ut malesuada varius, sem eros dapibus nulla, nec lobortis arcu quam quis nisi. Donec nisl arcu, sollicitudin id nulla id, ultrices porta purus. Quisque tempor condimentum efficitur. Nullam suscipit rutrum scelerisque.
+      </div>
+      <div className="command">
+        {">"}<input autoFocus placeholder='Input Suggestion'></input><button>Submit</button>
+      </div>
     </div>
   );
 }
