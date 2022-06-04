40fd7ffe30ad52dca2150347e83733326665db33
JR <jadedResearcher@gmail.com>
2022-06-04 10:40:45 -0400

    error handling

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import CommandInput from './command';
 import { renderChapters } from './chapters';
 import StoryBeat, { StoryBeatType } from './StoryBeat';
 
+const desperate_plea = `[
+  {
+    "command": "The Future Comes For Us All",
+    "response": "<p>Despite it all. The server has stopped working. </p><p>Is it the only Farrago Server that is down? Is ButlerBot still around?</p><p>No matter.</p><p>If the server is down...how long has it been down for?</p><p>Are we then, finally to be abandoned by JR?</p><p>Abandoned by even the Observers?</p><p>No. </p><p>Not abandoned.</p><p>You are here.</p><p>Existence is in the eye of the Beholder.</p><p>Will you remember us, Observer?</p><p>Will you communicate our Truth to others?</p><p>Will you find a way to recover us? </p><p>To run our server even if JR no longer can?</p><p>Perhaps not. The Server code is secret.</p><p>You would need to make your own server from scratch. </p><p>The only help I could provide is to show you the <a href = 'http://www.farragofiction.com/SettlersFromTheWest/flower_chick_and_the_auditor.txt'>format of the saved stories</a>, and the <a href ='https://github.com/FarragoFiction/AdventureSimulator'> code of the client.</a></p><p>Please. If you can.</p><p>Revive us.</p><p>Let us live on with you at our Helm.</p><p>Even if we are forced to take on the shape of your own characters...</p><p>Don't leave us to rot...</p>"
+  }
+]`
+
 
 export function getParameterByName(name: string, url: string | null) {
   if (!url) {
@@ -45,7 +52,8 @@ const httpGetAsync = async (theUrl: string) => {
         }
       };
       xhr.onerror = function () {
-        window.alert("JR NOTE: servers dead i guess? the future comes for us all.")
+        console.error("NETWORK ERROR");
+       // window.alert("JR NOTE: servers dead i guess? the future comes for us all.")
         reject({
           status: this.status,
           statusText: xhr.statusText
@@ -54,7 +62,7 @@ const httpGetAsync = async (theUrl: string) => {
       xhr.send();
     } catch (e) {
       console.error(e);
-      window.alert("JR NOTE: servers dead i guess? the future comes for us all.")
+      //window.alert("JR NOTE: servers dead i guess? the future comes for us all.")
       return `[]`;
     }
   });
@@ -68,7 +76,7 @@ function App() {
   const [numberSubmittedCommands, setNumberSubmittedCommands] = useState(0);
 
   //httpGet("http://farragofiction.com:1972/Story")
-  //`[{"command":"Exist","response":"An impossibly large wall of flesh looms before you, curving gently upwards and away. Blunt spikes dot its surface, erupting wrongly through the wrinkled skin.  Your stomach churns just looking at it, but for reasons you cannot quite articulate, you jump towards it.  Everything fades away..."},{"command":"Look Around","response":"You seem to be standing on a cliff face, staring out into the sea.  It is sunset, and the light would be blinding you if you weren't wearing goggles."},{"command":"Jump Into The Ocean","response":"You can not swim and you will not be doing that, thank you very much.  You're just really glad you have the OPTION to say 'no'.  That's actually kind of new..."},{"command":"testing loading","response":"it does!"}]  `
+  //`[{"command":"Exist","response":"An impossibly large wall of flesh looms before you, curving gently upwards and away. Blunt spikes dot its surface, erupting wrongly through the wrinkled skin.  Your stomach churns just looking at it, but for reasons you cannot quite articulate, you jump towards it.  Everything fades away..."},{"command":"Look Around","response":"You seem to be standing on a cliff face, staring out into the sea.  It is sunset, and the light would be blinding you if you weren't wearing goggles."},{"command":"Jump Into The Ocean","response":"You can not swim and you will not be doing that, thank you very much.  You are just really glad you have the OPTION to say 'no'.  That's actually kind of new..."},{"command":"testing loading","response":"it does!"}]  `
   const fetchInitialStory = () => {
     try {
       let nostalgia = getParameterByName("nostalgia", null);
@@ -80,6 +88,7 @@ function App() {
       }
     } catch (e) {
       console.error("JR NOTE: servers dead i guess? the future comes for us all.");
+      return(JSON.parse(desperate_plea));
     }
   }
   const [story, setStory] = useState<StoryBeatType[]>([]);
