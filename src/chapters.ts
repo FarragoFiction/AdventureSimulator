39edccd372c79f60a0b39da5fb487c8e09d40b58
JR <jadedResearcher@gmail.com>
2022-07-03 22:03:17 -0400

    tagline

--- src/chapters.ts
+++ src/chapters.ts
@@ -24,7 +24,7 @@ const chapters: Chapter[] = [
   new Chapter("Intermission 4", ["The End/ Camille"], "The consequences of your taunts...", false, "intermission3-end.txt"),
   new Chapter("Loop 3: Inexplicable Spiral", ["TODO"],  "'twitch chat defends itself in court, the long awaited sequel to twitch chat has a gun' --The Catalyst ", false, "inexplicable_spiral.txt"),
 
-  new Chapter("Current Savefile", ["TODO"], "'twitch chat defends itself in court, the long awaited sequel to twitch chat has a gun' --The Catalyst ", false, "current_save.txt"),
+  new Chapter("Current Savefile", ["TODO"], "What will you have Peewee do? ", false, "current_save.txt"),
 
 ]
 
